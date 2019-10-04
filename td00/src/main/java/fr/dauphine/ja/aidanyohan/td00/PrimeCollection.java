e0b4409b21bdee9d6eb13a76953ff3bd6ac0e793
aidayo18 <aidayo18@b042-14.crio.dauphine.fr>
2019-10-04 12:15:43 +0200

    initial import

---
+++ td00/src/main/java/fr/dauphine/ja/aidanyohan/td00/PrimeCollection.java
@@ -0,0 +1,60 @@
+package fr.dauphine.ja.aidanyohan.td00;
+
+import java.util.ArrayList;
+import java.lang.Math;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection 
+{
+	java.util.ArrayList<Integer> numbers;
+	
+	public PrimeCollection(ArrayList<Integer> numbers) {
+		super();
+		this.numbers = numbers;
+	}
+	
+public void initRandom(int n, int m) {
+	int nb;
+		for (int i=0; i<n ; i++) {
+			nb = (int) (Math.random() * m );
+			this.numbers.add(nb);
+			
+		}
+		
+	}
+public Boolean isPrime(int p) {
+	if(p==0) {
+		return false;
+	}
+	for (int i=2; i<p ; i++) {
+		if(p%i==0) {
+			return  false;
+		} 
+	}
+return true;
+	}
+
+public void printprimes() {
+	for(int i=0; i<this.numbers.size();i++) {
+		if(isPrime(this.numbers.get(i))) {
+			System.out.println(this.numbers.get(i));
+		}
+	}
+}
+
+public static void main( String[] args )
+{
+	ArrayList<Integer> numbers = new ArrayList<Integer>();
+	PrimeCollection p = new PrimeCollection(numbers);
+    p.initRandom(100, 1000);
+    System.out.println(p.numbers);  
+    System.out.println(p.isPrime(15)); 
+    p.printprimes();
+}
+}
+
+	
+	
