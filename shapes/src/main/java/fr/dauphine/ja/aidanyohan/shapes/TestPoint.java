47d8286dfbca06e80f20d8e7350cf3fb8a60b742
aidayo18 <aidayo18@b032-20.crio.dauphine.fr>
2019-10-11 14:42:14 +0200

    fin de l'exercice 2

---
+++ shapes/src/main/java/fr/dauphine/ja/aidanyohan/shapes/TestPoint.java
@@ -0,0 +1,14 @@
+package fr.dauphine.ja.aidanyohan.shapes;
+
+public class TestPoint {
+
+	private int x,y;
+	
+	
+	 public static void main( String[] args )
+	    {
+		 Point p=new Point();
+		 System.out.println(p.x+" "+p.y);
+	    }
+	
+}
