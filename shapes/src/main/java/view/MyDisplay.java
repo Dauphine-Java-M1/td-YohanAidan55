a592e1cd56f76ec08553a64e807fb783eab02faf
aidayo18 <aidayo18@b028-10.crio.dauphine.fr>
2019-11-05 13:28:22 +0100

    fin exo 1 td2

---
+++ shapes/src/main/java/view/MyDisplay.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MyDisplay extends JPanel{
+
+	@Override
+	 public void paintComponent(Graphics g){
+		super.paintComponent(g);
+	    //Vous verrez cette phrase chaque fois que la méthode sera invoquée
+	    System.out.println("Je suis exécutée !"); 
+	  
+	    g.drawLine(0,20,1000,2000);
+	    
+	  }  
+	
+	
+	public static void main(String []args){
+		JFrame frame = new JFrame("Java Avancé - Graphic Display");
+		frame.setSize(new Dimension(500,500));
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		MyDisplay d = new MyDisplay();
+		frame.add(d);
+		
+	}
+	
+}
+	
+	
+
